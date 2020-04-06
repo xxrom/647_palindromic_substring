b6ad30f4518b158a80dc46b4ed399f8125c40c85
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-06 21:53:04 +0300

    feat: dynamic solytion

--- main.py
+++ main.py
@@ -17,35 +17,68 @@ class Solution:
     return True
 
   def countSubstrings(self, s: str) -> int:
-    # ans = []
+    ans = []
     num = 0
 
     # print('str %s' % s)
 
     # print(self.checkIfPolindrom('abba', 0, 3))
 
-    # arr = [0] * len(s)
-    # for i in range(len(s)):
-    #   arr[i] = [0] * len(s)
-    # print(arr[i])
-    # for j in range(len(s)):
-    #   arr[i][j] = 'i= %d, j= %d' % (i, j)
-
-    # for i in range(len(s)):
-    #   for j in range(len(s)):
-    #     print('%s | ' % arr[i][j], end='')
-    #   print()
-
-    # print('____________')
+    arr = [0] * len(s)
     for i in range(len(s)):
+      arr[i] = [0] * len(s)
+      # print(arr[i])
+      # for j in range(len(s)):
+      #   arr[i][j] = 'i= %d, j= %d' % (i, j)
+
+    for i in range(len(s)):
+      for j in range(len(s)):
+        print('%s | ' % arr[i][j], end='')
+      print()
+
+    print('____________', len(s))
+    for i in range(len(s), -1, -1):
       for j in range(i, len(s)):
         # print(arr[i][j])
-        if self.checkIfPolindrom(s, i, j) == True:
-          # print('TRUE = %s' % s[i:j + 1])
-          num += 1
-          # ans.append(s[i:j + 1])
+        correct = self.checkIfPolindrom(s, i, j)
+        isPalindrom = False
 
-    # print('ans', ans)
+        if j == i:
+          isPalindrom = True
+        elif j - i == 1:
+          if s[i] == s[j]:
+            isPalindrom = True
+        else:  # j - i >= 2
+          #            cabbac
+          # If abbac |  10010
+          #                ^
+          #                ^
+          # Cabbac   | 100001
+          # abba == palindrom =>
+          # Cabbac - palindrom if abba - palindrom and
+          # first and last Chars are equal!
+          if arr[i + 1][j - 1] == 1 and s[i] == s[j]:
+            isPalindrom = True
+
+        if isPalindrom != correct:
+          print('WRONG!!!!!!!!', i, j)
+
+        if isPalindrom == True:
+          num += 1
+          arr[i][j] = 1
+          ans.append(s[i:j + 1])
+        # if self.checkIfPolindrom(s, i, j) == True:
+        #   # print('TRUE = %s' % s[i:j + 1])
+        #   num += 1
+        #
+        # ans.append(s[i:j + 1])
+
+    for i in range(len(s)):
+      for j in range(len(s)):
+        print('%s | ' % arr[i][j], end='')
+      print()
+
+    print('ans', ans)
     return num
     # return len(ans)
 
@@ -79,7 +112,6 @@ print("ans", ans)
 # b 0   1   0   0   1
 # e 0   0   0   0   0   1
 
-
 # ababaaaa
 
 #   a   b   a   b   a   a   a   a
@@ -89,8 +121,8 @@ print("ans", ans)
 # b 0   1   0   1
 # a 1   0   1   0   1
 # a 0   0   0   0   1   1
-# a 0   0   0   0   1   1
-# a 0   0   0   0   1   1
+# a 0   0   0   0   1   1   1
+# a 0   0   0   0   1   1   1   1
 #                   *   #
 
 # # => aAAA если aAAAa = полиндром
